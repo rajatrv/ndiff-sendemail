bc2dc52e663672b4bd6e6eaa35b46cf2e36d1ce4
Sam Gaudet <github@sgaudet.com>
2014-01-04 15:06:58 -0500

    added web script

--- ndiff_peeping.sh
+++ ndiff_peeping.sh
@@ -5,21 +5,21 @@
 # Variables
 TODAY=`date +%F`
 OUTPUT_DIR=$(dirname $0)/scans
-SCAN_BASE=$OUTPUT_DIR/baseline.xml
-SCAN_CURR=$OUTPUT_DIR/current.xml
-REPORT_FILE=$OUTPUT_DIR/ndiff.xml
+SCAN_BASE=$OUTPUT_DIR/baseline_web.xml
+SCAN_CURR=$OUTPUT_DIR/current_web.xml
+REPORT_FILE=$OUTPUT_DIR/ndiff_web.xml
 RECIPIENT='samuel.gaudet@maine.edu'
 NET_BLOCK='192.168.1.0/24'
  
 # Script
 touch $SCAN_BASE
-nmap -v -T4 -F -sV -oX $SCAN_CURR $NET_BLOCK
+nmap -sV -oX $SCAN_CURR $NET_BLOCK
 ndiff --xml $SCAN_BASE $SCAN_CURR > $REPORT_FILE
 
 # PeepingTom - LaNMaSteR53
-#python ./peepingtom.py -x /scans/ndiff.xml
+python ./peepingtom.py -x $REPORT_FILE
 
 # SendEmail - http://caspian.dotconf.net/menu/Software/SendEmail/
-./sendEmail -f "nmap@security-scanner2.iso.maine.edu" -t samuel.gaudet@maine.edu -s mail.maine.edu -u "Network diff report" -o message-file=/home/sgaudet/nmap/scans/scan_ndiff.txt
+#./sendEmail -f "nmap@security-scanner2.iso.maine.edu" -t samuel.gaudet@maine.edu -s mail.maine.edu -u "Network diff report" -o message-file=/home/sgaudet/nmap/scans/scan_ndiff.txt
 
 cp $SCAN_CURR $SCAN_BASE
---
+++ readme.md
@@ -0,0 +1,6 @@
+# network mapping scripts
+This repo has two (work in progress) flavors.
+  * ndiff.sh - create a report of new/removed hosts/services on an IP
+    range
+  * ndiff_web.sh - utilizing LaNMaSteR53's peepingtom, create report of
+    new web servers
